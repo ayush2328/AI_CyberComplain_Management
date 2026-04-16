7716b93a0fe2a9f506375952455d7f86e990905e
Ayush Gupta <ayushofficial.2328@gmail.com>
2026-04-17 01:36:53 +0530

    Add vercel.json for build config

--- next.config.js
+++ next.config.js
@@ -1,33 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     serverActions: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable TypeScript build errors for deployment
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  
-  // Disable ESLint build errors
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  
-  // Experimental features (fixed for Next.js 16)
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
-  },
-  
-  // Images configuration
-  images: {
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: { domains: ['avatars.githubusercontent.com'] },
 }
 
 module.exports = nextConfig
